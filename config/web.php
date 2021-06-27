5f025fe3fe91a2a597025ef30bb9e099b267dbba
Ammar dje <amardjebabla10@gmail..com>
2021-06-27 08:55:02 +0100

    fixing rule creating decaissement

--- config/web.php
+++ config/web.php
@@ -7,6 +7,7 @@ $config = [
     'id' => 'basic',
     'basePath' => dirname(__DIR__),
     'name' => 'Yalidine',
+    
     'defaultRoute' => 'user/security/login',
     'bootstrap' => ['log', 'user'],
     'aliases' => [
