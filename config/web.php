898a7d7cab451c5a1ecf1c0384b5a33944461896
Ammar dje <amardjebabla10@gmail..com>
2021-06-29 16:52:16 +0100

    work with ibrahim

--- config/web.php
+++ config/web.php
@@ -36,17 +36,6 @@ $config = [
         ],
 
         'errorHandler' => [
-            'errorAction' => 'site/error',
-        ],
-        'mailer' => [
-            'class' => yii\swiftmailer\Mailer::class,
-            // send all mails to a file by default. You have to set
-            // 'useFileTransport' to false and configure a transport
-            // for the mailer to send real emails.
-            'useFileTransport' => true,
-        ],
-        'log' => [
-            'traceLevel' => YII_DEBUG ? 3 : 0,
             'targets' => [
                 [
                     'class' => 'yii\log\FileTarget',
