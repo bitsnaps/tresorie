3d146ab95a98997cf643fea87a6e406a9ae5c61f
Ibrahim.H <bitsnaps@yahoo.fr>
2021-06-30 19:29:15 +0100

    update app title

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yalitresoredb',
+    'dsn' => 'mysql:host=127.0.0.1;dbname=yalitresoredb',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
