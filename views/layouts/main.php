5bfb82e8662cb89ed6bde78d94a1ef32f1dfac0a
Ibrahim.H <bitsnaps@yahoo.fr>
2021-07-02 15:41:00 +0100

    update home page content

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -80,9 +80,9 @@ AppAsset::register($this);
 
 <footer class="footer">
     <div class="container">
-        <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
+        <p class="pull-left">&copy; Yalidine <?= date('Y') ?></p>
 
-        <p class="pull-right"><?= Yii::powered() ?></p>
+        <p class="pull-right">Powered by CorpoSense</p>
     </div>
 </footer>
 
