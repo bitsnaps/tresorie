a9d9b2bc3700f139f3425e02083ae40a64dba5c8
raezon <amardjebabla10@gmail.com>
2021-07-04 13:00:51 +0100

    enhancing tests

--- config/db.php
+++ config/db.php
@@ -1,14 +1,7 @@
 <?php
+$db = require __DIR__ . '/db.php';
+// test database! Important not to run tests on production or development databases
 
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=127.0.0.1;dbname=yalitresoredb',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
+$db['dsn'] = 'mysql:host=127.0.0.1:3306;dbname=yalitresoredb_test';
 
-    // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
-];
+return $db;
