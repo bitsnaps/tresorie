d6a411dbe8e1d88b509b2ecfc9c698e880a14718
raezon <amardjebabla10@gmail.com>
2021-07-06 16:38:22 +0100

    documentating methodes

--- Query/kpiQuery.php
+++ Query/kpiQuery.php
@@ -6,18 +6,27 @@ use app\models\User;
 use app\models\AuthAssignment;
 use app\models\Decaissement;
 
-
-
 /**
  * This is the model class for table "grade".
  */
 class kpiQuery
 {
+    /**
+     * function to count user by role
+     *
+     * @param [type] $role_name
+     * @return int
+     */
     public static function countUserByRole($role_name){
         return AuthAssignment::find()->where(['item_name'=>$role_name])->count();
 
     }
     
+    /**
+     * function count number of decaisement
+     *
+     * @return int
+     */
     public static  function countDecaissement(){
        return  Decaissement::find()->count();
     }
