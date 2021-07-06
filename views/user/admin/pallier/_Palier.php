ddf1f13a85783322f20329a178b4331dd8edf552
raezon <amardjebabla10@gmail.com>
2021-07-06 11:00:03 +0100

    fixing merge issues

---
+++ views/user/admin/pallier/_Palier.php
@@ -0,0 +1,41 @@
+<?php
+use yii\helpers\ArrayHelper;
+use kartik\money\MaskMoney;
+/*
+ * This file is part of the 2amigos/yii2-usuario project.
+ *
+ * (c) 2amigOS! <http://2amigos.us/>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+/**
+ * @var yii\widgets\ActiveForm
+ * @var \Da\User\Model\User    $user
+ */
+?>
+<?= $form->field($grade, 'user_id')->dropDownList(
+
+ArrayHelper::map(
+
+app\models\AuthAssignment::find()->joinWith('user')->where(['item_name'=>"Approbateur"])->all(), 'user.id', function ($model) {
+ //   return ArrayHelper::toArray($model->user->username);
+    return $model->user->username;
+}
+),
+
+['prompt' => 'Sélectionner Utilisateur']);
+?>
+<?= $form->field($grade, 'role_id')->dropDownList(
+
+ArrayHelper::map(app\models\AuthItem::find()->where(['name'=>'Approbateur'])->all(), 'name', 'name'),
+
+['prompt' => 'Sélectionner Le Role']);
+?>
+<?= $form->field($grade, 'niveau')->textInput([  'placeholder' => 'Entrer  un niveau...','maxlength' => 255]) ?>
+<?= $form->field($grade, 'montant')->textInput([  'placeholder' => 'Entrer  un Montant...','type' => 'number','min'=>0, 'options' => [
+      
+        'min'=>0,
+        'style' => 'width:300 px'
+    ]]) ?>
---
+++ views/user/admin/pallier/allPaliers.php
@@ -0,0 +1,67 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+use yii\widgets\Pjax;
+use app\models\User;
+use app\models\Role;
+use kartik\money\MaskMoney;
+/* @var $this yii\web\View */
+/* @var $searchModel app\models\GradeSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = Yii::t('app', 'Tous les Paliers');
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<?php $this->beginContent('@app/views/user/shared/admin_layout_Palier.php') ?>
+<div class="grade-index">
+
+
+
+    <?php Pjax::begin(); ?>
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'summary'=>'', 
+        'columns' => [
+            [
+                'attribute' => 'userID',
+                'label' => 'Utilisateur',
+                'value' => 'user.username'
+                
+    
+            ],
+            [
+                'attribute' => 'roleID',
+                'label' => 'Role',
+                'value' => 'role.role_name'
+            ],
+          //  'niveau',
+            [
+                'attribute' => 'montant',
+                'format' => 'raw',
+                'label' => 'Montant',
+               
+                'value' => function ($model) {
+                    
+                    return $model->montant.' DA';
+                },
+            ],
+          
+          
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+
+    <?php Pjax::end(); ?>
+
+</div>
+<?php $this->endContent() ?>
+<?php
+$this->registerJs('
+$("body").on("keyup.yiiGridView", ".grid-view .filters input", function(){
+    $(".grid-view").yiiGridView("applyFilter");
+})', \yii\web\View::POS_READY);
+?>
